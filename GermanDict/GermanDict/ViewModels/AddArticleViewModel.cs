037b3300cecbf61b35656627ab25537cd5023361
Norbert Barna <norbert.barna@heidelberg-instruments.com>
2023-12-13 11:02:03 +0100

    updated usercontrols

--- GermanDict/GermanDict/ViewModels/AddArticleViewModel.cs
+++ GermanDict/GermanDict/ViewModels/AddArticleViewModel.cs
@@ -1,12 +1,64 @@
-﻿
+﻿using GermanDict.Interfaces;
+using System.Collections.ObjectModel;
+
 namespace GermanDict.ViewModels
 {
     public class AddArticleViewModel : ViewModelBase
     {
+        
 
         public AddArticleViewModel()
         {
             Name = "AddArticle";
         }
+
+
+        private IArticle _selectedArticle;
+        public IArticle SelectedArticle
+        {
+            get { return _selectedArticle; }
+            set
+            {
+                _selectedArticle = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private ObservableCollection<IArticle> _availableArticles;
+        public ObservableCollection<IArticle> AvailableArticles
+        {
+            get { return _availableArticles; }
+            set
+            {
+                _availableArticles = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private Language _selectedLanguage;
+        public Language SelectedLanguage
+        {
+            get { return _selectedLanguage; }
+            set
+            {
+                _selectedLanguage = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private ObservableCollection<Language> _availableLanguages;
+        public ObservableCollection<Language> AvailableLanguages
+        {
+            get { return _availableLanguages; }
+            set
+            {
+                _availableLanguages = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
--- GermanDict/WordDBRepository/WordDBRepository.cs
+++ GermanDict/WordDBRepository/WordDBRepository.cs
@@ -2,42 +2,43 @@
 
 namespace GermanDict.WordDBRepository
 {
-    public class WordDBRepository : IRepository<IWord>
+    public class WordDBRepository<T> : IRepository<T>
+        where T : IRepositoryElement
     {
 
         #region IRepository<IWord>
 
-        public void Add(IWord word)
+        public void Add(T word)
         {
             throw new NotImplementedException();
         }
 
-        public void AddRange(IEnumerable<IWord> words)
+        public void AddRange(IEnumerable<T> words)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<IWord> Find(Predicate<IWord> predicate)
+        public IEnumerable<T> Find(Predicate<T> predicate)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<IWord> Get(string text)
+        public IEnumerable<T> Get(string text)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<IWord> GetAllElements()
+        public IEnumerable<T> GetAllElements()
         {
             throw new NotImplementedException();
         }
 
-        public void Remove(IWord word)
+        public void Remove(T word)
         {
             throw new NotImplementedException();
         }
 
-        public void RemoveRange(IEnumerable<IWord> words)
+        public void RemoveRange(IEnumerable<T> words)
         {
             throw new NotImplementedException();
         }
